42da225085edb96e454c8d414ed781b481b91743
raut-abhishek <iamabhishekraut01@gmail.com>
2026-02-25 17:14:52 +0530

    initial server setup

--- server/index.js
+++ server/index.js
@@ -1,4 +1,26 @@
-import express from 'express';
-import cors from 'cors'
-import dotenv from 'dotenv';
-import conneDB from './config/db.js';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+
+// Connect DB
+await connectDB();
+
+// Global Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Health Route
+app.get("/", (req, res) => {
+  res.json({ message: "AI Job Tracker API Running..." });
+});
+
+const PORT = process.env.PORT ?? 5000;
+
+app.listen(PORT, () =>
+  console.log(`Server running on port ${PORT}`)
+);
